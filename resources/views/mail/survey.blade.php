398ce2c7d9cd1a080538a0e47500436b05f452c5
dlinox <nearlino20@gmail.com>
2024-09-09 06:51:45 -0500

    demo

--- resources/views/mail/survey.blade.php
+++ resources/views/mail/survey.blade.php
@@ -13,7 +13,8 @@
 
     <p>Estimado/a </p>
     <p>Gracias por confiar en nosotros. Nos gustaría saber cómo ha sido su experiencia con nuestro servicio. Por favor, haga clic en el siguiente enlace para completar una breve encuesta de satisfacción:</p>
-    <a href="https://sso.linox.net.pe/c/survey/{{ $token }}">Realizar encuesta</a>
+    <!-- <a href="https://sso.linox.net.pe/c/survey/{{ $token }}">Realizar encuesta</a> -->
+    <a href="http://demo-sso.linox.net.pe/c/survey/{{ $token }}">Realizar encuesta</a>
     <!-- <a href="http://localhost:5173/c/survey/{{ $token }}">Realizar encuesta</a> -->
     <p>Gracias por su colaboración.</p>
     <p>Saludos cordiales,</p>
